6b8c685ad858d70f610d8fd7e60b3a2739e89fc5
Jithin <jithinpulikkal875@gmail.com>
2024-01-01 18:04:41 +0530

    skills

--- src/content/skills.js
+++ src/content/skills.js
@@ -3,15 +3,20 @@ const skills = [
   "css.svg",
   "bootstrap.svg",
   "js.svg",
+  "jquery.svg",
   "nodejs.svg",
   "express.svg",
   "react.svg",
+  "redux.svg",
   "mongodb.svg",
-  
+  "mysql.svg",
+  "postgresql.svg",
   "git.svg",
   "github.svg",
- 
+  "postman.svg",
+  "npm.svg",
   "tailwind.svg",
+  "aws.svg",
 
 ]; // Add or remove skills here, make sure to include the image respective to the skill inside the public/skills folder and use the correct file extension
 
